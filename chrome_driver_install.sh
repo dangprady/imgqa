a1511fbe98dd10671caa0d16e838f36918c6706a
dangprady <pradeep.pyc@gmail.com>
2019-01-18 20:13:44 +0530

    Added support for macOS for chromedriver download and installation

--- chrome_driver_install.sh
+++ chrome_driver_install.sh
@@ -1,8 +1,22 @@
 # Install ChromeDriver.
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
+if which curl >/dev/null; then
+	CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+else
+	echo "curl is required, please install curl"
+	exit 1
+fi
+
+if [[ $(uname) == 'Linux' ]]; then
+	os_name='linux64'
+elif [[ $(uname) == 'Darwin' ]]; then
+	os_name='mac64'
+else
+	echo "OS chould not be recognized Exiting........"
+        exit 1
+fi
+wget -N http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_${os_name}.zip -P ~/
+unzip ~/chromedriver_${os_name}.zip -d ~/
+rm ~/chromedriver_${os_name}.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
+
